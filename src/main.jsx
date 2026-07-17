c988259d846bcf1d3c9360ca9de8f3d1955ab997
AbdElRhman-Sabry59 <abdo.sabry552002@gmail.com>
2026-07-17 17:40:17 +0300

    fix basename

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/portfolio_by_react/">
       <App />
     </BrowserRouter>
   </StrictMode>,
